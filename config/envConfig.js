2a9484c3613e43d553ede609ce8305a4e7325f9e
spencer <spencer.bigum@gmail.com>
2017-05-18 02:47:27 -0400

    fix app v1

--- config/envConfig.js
+++ config/envConfig.js
@@ -3,15 +3,15 @@ const prod = process.env.NODE_ENV === 'production'
 
 export default {
   BACKEND_URL: prod
-    ? 'https://nameless-scrubland-28835.herokuapp.com'
+    ? 'https://node-intro-api.herokuapp.com/'
     : 'http://localhost:7777',
   WEBSITE_TITLE: 'Now Thats Delicious!',
   LINKS: [
     { slug: '/stores', title: 'Stores', icon: svgs.Store },
     { slug: '/tags', title: 'Tags', icon: svgs.Tag },
-    { slug: '/top', title: 'Top', icon: svgs.Top },
-    { slug: '/create', title: 'Add', icon: svgs.Add },
-    { slug: '/map', title: 'Map', icon: svgs.MapSvg }
+    // { slug: '/top', title: 'Top', icon: svgs.Top },
+    { slug: '/create', title: 'Add', icon: svgs.Add }
+    // { slug: '/map', title: 'Map', icon: svgs.MapSvg }
   ],
   LOGO: svgs.Logo,
   TAGS: ['Wifi', 'Open Late', 'Family Friendly', 'Vegetarian', 'Licensed']
